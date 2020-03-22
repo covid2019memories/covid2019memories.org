6937bf972a3166156bdbb8ab427223e1eb183ecd
Mingli Yuan <mingli.yuan@gmail.com>
2020-03-22 12:35:12 +0800

    add skeleton

---
+++ src/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, escape, request, render_template
+
+app = Flask(__name__, static_folder='web/static', static_url_path='', template_folder='web/templates')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', **{
+        'ulang': 'en',
+        'perspective': 'datealigned',
+        'languages': 'any',
+        'countries': 'cn+kr+ja+it+ir',
+        'categories': 'wrd+plt+bsn+spt+clt+tch+sci+hlt+opn+psn',
+        'gourpby': 'country',
+        'time': 20,
+    })
+
+
+@app.route('<ulang>/index/<perspective>/<languages>/<countries>/<categories>/<gourpby>/<time>')
+def index(ulang, perspective, languages, countries, categories, gourpby, time):
+    return render_template('index.html', **{
+        'ulang': ulang,
+        'perspective': perspective,
+        'languages': languages,
+        'countries': countries,
+        'categories': categories,
+        'gourpby': gourpby,
+        'time': time,
+    })
+
+
+@app.route('/<ulang>/post/<pid>')
+def post():
+    return render_template('post.html')
+
+
+@app.route('/<ulang>/translation/<tid>')
+def translation():
+    return render_template('translation.html')
+
